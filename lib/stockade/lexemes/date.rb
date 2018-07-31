f317c94713c25658e757b7309a009fb3bf58021c
Stan Mazhara <akmegran@gmail.com>
2018-07-30 20:40:58 -0700

    Upgrade dob recognition

--- lib/stockade/lexemes/date.rb
+++ lib/stockade/lexemes/date.rb
@@ -7,13 +7,13 @@ module Stockade
       class << self
         def regex
           /
-          (?:
-            \d{1,2}
+          (?<!\d)
+           (\d{1,2})
             #{delim}
-            \d{1,2}
+          (\d{1,2})
             #{delim}
-            \d{4}
-          )
+          ((?:19|20)\d{1,2})
+          (?!\d)
           /x
         end
 
@@ -23,7 +23,25 @@ module Stockade
       end
 
       def valid?
-        true
+        possible_dates.any? &&
+        possible_dates.all? do |date|
+          date <= ::Date.today
+        end
+      end
+
+      private
+
+      def possible_dates
+        parts.permutation.map do |permutation|
+          begin
+            ::Date.new(*permutation)
+          rescue ArgumentError
+          end
+        end.compact
+      end
+
+      def parts
+        self.class.regex.match(value)[1..-1].map(&:to_i)
       end
     end
   end
